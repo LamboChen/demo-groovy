a1f5ee0277313710ccecd2333357b4f65c3f872b
lambochen <lambochen@yeah.net>
2022-04-15 10:14:25 +0800

    example

---
+++ src/main/java/cn/lambochen/demo/groovy/grammar/Example.groovy
@@ -0,0 +1,16 @@
+package cn.lambochen.demo.groovy.grammar
+
+class Example {
+
+    static void main(String[] args) {
+        def x = 5
+        def y = 6L
+        def z = "hello world"
+
+        def bi = 1g
+        def bd = 3.5g
+
+        println x + y + z
+        println bi + bd
+    }
+}
---
+++ src/test/java/cn/lambochen/demo/groovy/DemoGroovyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lambochen.demo.groovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGroovyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
